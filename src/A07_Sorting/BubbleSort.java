67da0bb68b576a6f1394e4bbf42b35f0a9aea222
skochauf <stephan.kochauf@lv.campus02.at>
2022-12-02 17:39:45 +0100

    BubbleSort

--- src/A07_Sorting/BubbleSort.java
+++ src/A07_Sorting/BubbleSort.java
@@ -8,6 +8,24 @@ public class BubbleSort implements PersonenSort {
 	 */
 	public void sort(Person[] personen) {
 
+		// O(n²)
+
+		for (int i = 0; i < personen.length; i++) {
+			boolean anyChange = false;
+			for (int j = 0; j < personen.length - 1 - i; j++) { // codierfehler
+
+				if (personen[j].compareTo(personen[j + 1]) > 0){
+					Person temp = personen[j];
+					personen[j] = personen[j + 1];
+					personen[j + 1] = temp;
+					anyChange = true;
+				}
+			}
+			if (anyChange == false) {
+				break;
+			}
+		}
+
 	}
 	
 }
--- src/A07_Sorting/QuickSort.java
+++ src/A07_Sorting/QuickSort.java
@@ -9,5 +9,13 @@ public class QuickSort implements PersonenSort {
 	public void sort(Person[] personen) {
 
 	}
-	
+
+	private void sort(Person[] personen, int left, int right) {
+
+	}
+
+	private int partition(Person[] personen, int left, int right) {
+
+		return 0;
+	}
 }
