edbb54488c67aba4c2b552123ccd4cb797471ef0
skochauf <stephan.kochauf@lv.campus02.at>
2022-12-02 19:27:26 +0100

    Implementierung Quicksort

--- src/A07_Sorting/QuickSort.java
+++ src/A07_Sorting/QuickSort.java
@@ -7,15 +7,44 @@ public class QuickSort implements PersonenSort {
 	 * Sortier-Funktion
 	 */
 	public void sort(Person[] personen) {
-
+		sort(personen, 0, personen.length - 1);
 	}
 
 	private void sort(Person[] personen, int left, int right) {
+		if (left >= right)
+			return;
 
+		int p = partition(personen, left, right); // Pivotelement an die richtige Position verschieben
+		sort(personen, left, p - 1);
+		sort(personen, p + 1, right);
 	}
 
 	private int partition(Person[] personen, int left, int right) {
 
-		return 0;
+		Person pivotPerson = personen[right];
+		int i = left - 1; // wieso -1
+		int k = right;
+
+		do {
+			do {
+				i++;
+			} while (personen[i].compareTo(pivotPerson) < 0);
+
+			do {
+				k--;
+			} while (k >= 0 && personen[k].compareTo(pivotPerson) > 0); // fehler
+
+			if (i < k) { // nicht überholt
+				Person temp = personen[i];
+				personen[i] = personen[k];
+				personen[k] = temp;
+			}
+		} while (i < k);
+
+		Person temp = personen[i];
+		personen[i] = pivotPerson;
+		personen[right] = temp;
+
+		return i;
 	}
 }
